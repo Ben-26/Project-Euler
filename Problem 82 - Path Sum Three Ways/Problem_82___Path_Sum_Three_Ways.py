498c0c2eb1c93fa5d06885f9351b028a5edc8c72
Ben-26 <ben.l.mclaren@icloud.com>
2024-09-13 16:44:23 +0100

    path sum wizard
    very fun sols

--- Problem 82 - Path Sum Three Ways/Problem_82___Path_Sum_Three_Ways.py
+++ Problem 82 - Path Sum Three Ways/Problem_82___Path_Sum_Three_Ways.py
@@ -1,26 +1,25 @@
-import numpy as np
 
 def main():
-    with open("0082_matrix.txt", "r") as matrix_file:
-        matrix = np.array([[int(char) for char in row.rstrip().rsplit(",")] for row in matrix_file])
-    
-    print(matrix)
+    matrix = []
+    matrix_file = open("0082_matrix.txt", "r")
+    for row in matrix_file:
+        matrix.append([int(x) for x in row.rstrip().split(",")])  
 
+    nrows = len(matrix)
+    ncols = len(matrix[0])
+
+    path_sum = [[(matrix[row][0] if col == 0 else 0) for col in range(ncols)] for row in range(nrows)]
+    
+    for col in range(1, ncols):
+        path_sum[0][col] = matrix[0][col] + path_sum[0][col - 1]
+        
+        for _ in range(2):
+            for row in range(nrows):
+                path_sum[row][col] = matrix[row][col] + min(path_sum[row - 1][col], path_sum[row][col - 1])
+            for row in range(nrows - 2, -1, -1):
+                path_sum[row][col] = min(path_sum[row + 1][col] + matrix[row][col], path_sum[row][col])
+
+    print(min([path_sum[i][ncols - 1] for i in range(nrows)]))
 
 if __name__ == "__main__":
     main()
-
-    """
-        # Testing
-    matrix = [[99, 390, 42, 1007, 55],
-     [999, 39, 4, 17, 108],
-     [131, 3, 4, 10, 1008],
-     [1, 96, 342, 9, 150],
-     [630, 803, 746, 2, 111],
-     [537, 4, 497, 1, 6],
-     [805, 3, 524, 37, 101],
-     [5, 3, 24, 37, 101]] 
-    
-
-    
-    """
--- Problem 83 - Path Sum Four Ways/Problem_83___Path_Sum_Four_Ways.py
+++ Problem 83 - Path Sum Four Ways/Problem_83___Path_Sum_Four_Ways.py
@@ -1,15 +1,60 @@
 
-
-
-
-
 def main():
     matrix = []
     matrix_file = open("0083_matrix.txt", "r")
     for row in matrix_file:
-        matrix.append([int(x) for x in row.rstrip().split(",")])  
+        matrix.append([float('inf')] + [int(x) for x in row.rstrip().split(",")] + [float('inf')]) 
+    nrows = len(matrix)  
+    f = [float('inf') for _ in range(nrows + 2)]
+    matrix = [f] + matrix + [f]
 
+    nrows += 2
+    ncols = len(matrix[0])
+    
+    path_sum = [[float('inf') for _ in range(ncols)] for _ in range(nrows)]
+    path_sum[1][1] = matrix[1][1]
 
+    for l in range(1, nrows - 1):
+        for _ in range(2):
+            row, col = 0, l
+        
+            while row != col:
+                # Col down - cmp above, left and self
+                path_sum[row][col] = min(path_sum[row][col - 1] + matrix[row][col],
+                                         path_sum[row - 1][col] + matrix[row][col],
+                                         path_sum[row][col])
+                row += 1
+
+            while col >= 0:
+                # Row left - cmp above, right and self
+                path_sum[row][col] = min(path_sum[row - 1][col] + matrix[row][col],
+                                         path_sum[row][col + 1] + matrix[row][col],
+                                         path_sum[row][col])
+                col -= 1
+           
+            while row != col:
+                # Row right - cmp above, left and self
+                path_sum[row][col] = min(path_sum[row - 1][col] + matrix[row][col],
+                                         path_sum[row][col - 1] + matrix[row][col],
+                                         path_sum[row][col])
+                col += 1
+
+            while row >= 0:
+                # Col up - cmp below, left and self
+                path_sum[row][col] = min(path_sum[row + 1][col] + matrix[row][col],
+                                         path_sum[row][col - 1] + matrix[row][col],
+                                         path_sum[row][col])
+                row -= 1
+
+    for row in range(1, nrows - 1):
+        for col in range(1, ncols - 1):
+            path_sum[row][col] = min(path_sum[row - 1][col] + matrix[row][col],
+                                     path_sum[row + 1][col] + matrix[row][col],
+                                     path_sum[row][col + 1] + matrix[row][col],
+                                     path_sum[row][col - 1] + matrix[row][col],
+                                     path_sum[row][col])
+    print(path_sum[-2][-2])
 
 if __name__ == "__main__":
     main()
+
